37f8d1038f2b4615bfd758c2a78c4368a8399774
JSARVER <G240b583>
2020-10-29 20:17:51 -0700

    add config_from_file function

--- cherpy/__init__.py
+++ cherpy/__init__.py
@@ -1,1 +1,2 @@
-from .main import config_from_env,update_object_from_file, extract_data, get_object_schema
+from .main import update_object_from_file, extract_data, get_object_schema
+from .api import config_from_env
