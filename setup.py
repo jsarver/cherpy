372eb3893df61d2e86091cacc4b1850e384ce7aa
JSARVER <G240b583>
2020-10-29 20:19:11 -0700

    added automatic token refresh client class

--- setup.py
+++ setup.py
@@ -10,22 +10,22 @@ setup(
     author='JSARVER',
     author_email='',
     description='',
-    install_requires=["""attrs==17.2.0
-                        certifi==2017.7.27.1
-                        chardet==3.0.4
-                        click==6.7
-                        idna==2.5
-                        python-dateutil==2.6.1
-                        pytz==2017.2
-                        PyYAML==4.2b1
-                        requests==2.20.0
-                        six==1.10.0
-                        urllib3==1.23"""
+    install_requires=["""attrs
+                        chardet
+                        click
+                        idna
+                        python-dateutil
+                        pytz
+                        PyYAML
+                        requests
+                        six
+                        urllib3
+                        loguru
+                        """
                       ],
     entry_points="""
     [console_scripts]
-    query_object=cherpy.scripts.query_object:cli
-    delete_objects=cherpy.scripts.delete_object:delete_cli
+    serviceinfo=cherpy.api:service_info
     update_object=cherpy.scripts.update_from_file:update_object_from_file_cli
     
     """,
