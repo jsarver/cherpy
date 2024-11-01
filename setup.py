bbe34ff58376be3c277dc16007bb1711ff9a05cb
Josh Sarver <jsarver@lhs.org>
2024-11-01 15:09:59 -0700

    renamed cli_tools.py to cli.py and updated entry points for cli commands

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     url='',
     license='',
-    author='Josh sarver',
+    author='Josh Sarver',
     author_email='josh.sarver@gmail.com',
     description='A rest api wrapper for the Cherwell Service management platform',
     install_requires=["""attrs
@@ -18,4 +18,14 @@ setup(
                         """
                       ],
     python_requires='>=3.9',
+    entry_points="""
+        [console_scripts]
+        search=cherpy.cli:search_object_cli
+        delete_objects=cherpy.cli:delete_objects_cli
+        update=cherpy.cli:update_object_cli
+        create=cherpy.cli:create_object
+        run-onestep=cherpy.cli:run_onestep_cli
+        csm=cherpy.cli:csm
+        """
+
 )
