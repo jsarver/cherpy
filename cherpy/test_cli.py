452d5e0c0af0e647c5515aa1ab00909c91a79412
Josh Sarver <jsarver@lhs.org>
2024-11-04 17:00:12 -0800

    added test_cli.py

---
+++ cherpy/test_cli.py
@@ -0,0 +1,14 @@
+# test that the CLI works using click testing
+from click.testing import CliRunner
+
+from cherpy.cli import search_object_cli
+
+
+def test_search_object_cli():
+    runner = CliRunner()
+    result = runner.invoke(search_object_cli,
+                           ['--env', 'cherpy_dev', '--object-name', 'Incident', '--page-size', '10', '--output-file',
+                            'test.csv'])
+    assert result.exit_code == 0
+    with open('test.csv', 'r') as f:
+        assert f.read() != ""
