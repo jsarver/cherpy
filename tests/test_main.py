79e6bfa6a2311a0c66843e4ae4b497ccbed26167
Josh Sarver <jsarver@lhs.org>
2024-11-04 16:52:33 -0800

    add search_object test

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,8 +5,7 @@ from cherpy.main import search_object
 c = config_from_env("cherpy_dev")
 c.login()
 
-# confirm that object query works
-@pytest.fixture()
+
 def test_search_object():
     response = search_object(c, object_name="Incident",
                              pageSize=10)
